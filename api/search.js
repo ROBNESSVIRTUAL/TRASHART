194764be677edab6f6235d790ca3ba1c8e214882
ROBNESS <80085617+ROBNESSVIRTUAL@users.noreply.github.com>
2026-05-20 22:46:05 -0700

    Update search.js

--- api/search.js
+++ api/search.js
@@ -3,9 +3,9 @@
 // POST request with { query, chain, maxPrice }
 // Returns array of matching TRASHART NFTs
 
-import { sql } from '@vercel/postgres';
+const { sql } = require('@vercel/postgres');
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
